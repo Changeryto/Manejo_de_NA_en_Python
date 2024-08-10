b8b540070d9aeb9aef6a153bd348bf65e94751f7
Changeryto <changeryto@hotmail.com>
2024-08-10 15:36:18 -0600

    Se han aniadido multiples funciones para manejo de NAs, y sus test

--- notebooks/scripts/pandas_missing_extension.py
+++ notebooks/scripts/pandas_missing_extension.py
@@ -16,30 +16,65 @@ class MissingMethods:
     def __init__(self, pandas_obj):
         self._df = pandas_obj
 
-    def na_full_count(self):
-        return self._df.isna().sum().sum()
-
-    def not_na_full_count(self):
-        return self._df.size - self._df.missing.na_full_count()
+    def na_count(self, *var):
+        """Returns the total of NAs in the whole Data Frame"""
+        if len(var) == 0:
+            return self._df.isna().sum().sum()
+        elif len(var) > 1:
+            raise ValueError(f"Use only one valid variable name of Data Frame")
+        else:
+            return self._df[var[0]].isna().sum()
 
     def na_proportion(self):
-        return (self.na_full_count() / self._df.size)
+        """Returns the proportion (from 0 to 1) of NAs in the whole the Data Frame"""
+        return (self.na_count() / self._df.size)
+    
+    def na_percentage(self):
+        """Returns the percentage (from 0 to 100) of NAs in the whole Data Frame"""
+        return (self.na_proportion() * 100)
+
+    def not_na_count(self):
+        """Returns the total of not NAs in the whole Data Frame"""
+        return self._df.size - self._df.missing.na_count()
+
+    def not_na_proportion(self):
+        """Returns the proportion (from 0 to 1) of not NAs in the whole the Data Frame"""
+        return 1 - self.na_proportion()
+
+    def not_na_percentage(self):
+        """Returns the percentage (from 0 to 100) of not NAs in the whole Data Frame"""
+        return 100 - self.na_percentage()
+
+    def variable_summary(self):
+        """Returns a Pandas Data Frame with: variable (Name of each column), n_cases (Number of rows for each variable), n_nas (Number of NAs for each variable), n_availables (Number of not NAs for each variable), na_proportion (proportion of not NAs for each variable), na_percentage (Percentage of NAs for each variable)"""
+        return pd.DataFrame.from_dict({
+            "variable": self._df.columns,
+            "n_cases": self._df.shape[0],
+            "n_nas": self._df.isna().sum().to_numpy(),
+            "n_availables": self._df.notna().sum().to_numpy(),
+            "na_proportion": (self._df.isna().sum().to_numpy() / self._df.shape[0]),
+            "na_percentage": (self._df.isna().sum().to_numpy() / self._df.shape[0]) * 100
+        })
+
+    def na_summary_table(self):
+        """Returns a Pandas Data Frame with: n_na (Number of NAs in any variable), n_variables (number of variables with that NAs), percentage_of_vars (Percentage of variables with that number of NAs)"""
 
 
-# Para crear un Data Frame que pueda rescatarse
 
-DF_TEST_MISSIGN = pd.DataFrame.from_dict(
-    data = {
-        "a": list("asdafsdafa"),
-        "b": range(0, 10)
-    }
-    )
-DF_TEST_MISSIGN.iloc[2:5, 0] = None
-DF_TEST_MISSIGN.iloc[6:7, 1] = None
+
 
 if __name__ == "__main__":
+    # Para crear un Data Frame que pueda rescatarse
+
+    DF_TEST_MISSIGN = pd.DataFrame.from_dict(
+        data = {
+            "a": list("asdafsdafa"),
+            "b": range(0, 10)
+        }
+    )
+    DF_TEST_MISSIGN.iloc[2:5, 0] = None
+    DF_TEST_MISSIGN.iloc[6:7, 1] = None
     print("Probando la extension")
-    print(DF_TEST_MISSIGN.missing.na_proportion())
 
 
 
--- notebooks/scripts/tests.py
+++ notebooks/scripts/tests.py
@@ -1,17 +1,51 @@
 #!../../env/bin/python
 import unittest
+import pandas as pd
 import pandas_missing_extension
-from pandas_missing_extension import DF_TEST_MISSIGN
+
+
+# UN DATA FRAME PARA TESTEAR
+DF_TEST_MISSIGN = pd.DataFrame.from_dict(
+    data = {
+        "a": list("asdafsdafa"),
+        "b": range(0, 10)
+    }
+)
+DF_TEST_MISSIGN.iloc[2:5, 0] = None
+DF_TEST_MISSIGN.iloc[6:7, 1] = None
+
+
 class TestMissingMethods(unittest.TestCase):
 
-    def test_na_full_count(self):
-        self.assertEqual(DF_TEST_MISSIGN.missing.na_full_count(), 4)
-
-    def test_na_full_count(self):
-        self.assertEqual(DF_TEST_MISSIGN.missing.not_na_full_count(), 16)
+    def test_na_count(self):
+        self.assertEqual(DF_TEST_MISSIGN.missing.na_count(), 4)
+    
+    def test_arged_na_count(self):
+        self.assertEqual(DF_TEST_MISSIGN.missing.na_count("a"), 3)
 
     def test_na_proportion(self):
         self.assertEqual(DF_TEST_MISSIGN.missing.na_proportion(), 0.2)
 
+    def test_na_percentage(self):
+        self.assertEqual(DF_TEST_MISSIGN.missing.na_percentage(), 20)
+
+    def test_not_na_proportion(self):
+        self.assertEqual(DF_TEST_MISSIGN.missing.not_na_proportion(), 0.8)
+
+    def test_not_na_percentage(self):
+        self.assertEqual(DF_TEST_MISSIGN.missing.not_na_percentage(), 80)
+
+    def test_variable_summary(self):
+        wanted = pd.DataFrame.from_dict({
+            "variable": ("a", "b"),
+            "n_cases": (10, 10),
+            "n_nas": (3, 1),
+            "n_availables": (7, 9),
+            "na_proportion": (0.3, 0.1),
+            "na_percentage": (30.0, 10.0)
+        })
+        self.assertEqual(DF_TEST_MISSIGN.missing.variable_summary().to_dict(), wanted.to_dict())
+
 if __name__ == "__main__":
+    print(DF_TEST_MISSIGN)
     unittest.main()
